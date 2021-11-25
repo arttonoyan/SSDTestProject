9bc0760d642b1979286c4a9837b45c7fe68907ef
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-11-25 15:54:14 +0400

    Create project

---
+++ Database/Database/dbo/Tables/Student.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [Id]      INT            IDENTITY (1, 1) NOT NULL,
+    [Name]    NVARCHAR (MAX) NOT NULL,
+    [Surname] NVARCHAR (MAX) NOT NULL,
+    [Age]     INT            NOT NULL,
+    [Gender]  TINYINT        DEFAULT ((1)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
